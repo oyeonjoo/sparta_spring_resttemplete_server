15e6fa2e45e63515179c002a210e0acf8ca49a75
oyeonjoo <oyj0842@naver.com>
2024-02-17 02:11:43 +0900

    RestTemplate getForEntity 요청

--- src/main/java/com/sparta/springresttemplateserver/SpringRestTemplateServerApplication.java
+++ src/main/java/com/sparta/springresttemplateserver/SpringRestTemplateServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringResttemplateServerApplication {
+public class SpringRestTemplateServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringResttemplateServerApplication.class, args);
+		SpringApplication.run(SpringRestTemplateServerApplication.class, args);
 	}
 
 }
--- src/test/java/com/sparta/springresttemplateserver/SpringRestTemplateServerApplicationTests.java
+++ src/test/java/com/sparta/springresttemplateserver/SpringRestTemplateServerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringResttemplateServerApplicationTests {
+class SpringRestTemplateServerApplicationTests {
 
 	@Test
 	void contextLoads() {
